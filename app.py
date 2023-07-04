ede676e738ce4fb38561a1cb226989554131271e
Bear does coding <105804085+Akari-ii@users.noreply.github.com>
2023-07-04 13:14:50 +0800

    initial database

--- app.py
+++ app.py
@@ -1,37 +1,60 @@
 from __future__ import unicode_literals, print_function, division
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3 as db
+from cryptography.fernet import Fernet
+import pyrebase as db
 import FrenchToEngModel as En
 import EngToFrenchModel as Fr
-
 app = Flask(__name__)
 
 
-#print("Opened database successfully")
-#conn = db.connect('test.db')
-#conn.execute('''CREATE TABLE ACC
-#         (USER TEXT PRIMARY KEY     NOT NULL,
-#        PASSWORD           TEXT    NOT NULL,
-#        HISTORY        CHAR(50));''')
-#print("Table created successfully")
+config = {"apiKey": "AIzaSyDkiF2t9twh5qfFMziliS7b9nI6pX2c_ag",
+          "authDomain": "babelbot-f8aaf.firebaseapp.com",
+          "databaseURL": "https://babelbot-f8aaf-default-rtdb.asia-southeast1.firebasedatabase.app",
+          "projectId": "babelbot-f8aaf",
+          "storageBucket": "babelbot-f8aaf.appspot.com",
+          "messagingSenderId": "761927119070",
+          "appId": "1:761927119070:web:fb8be847d84d3d58f8524f",
+          "measurementId": "G-ZEDQLD365N"
+}
 
-#conn.close()
+firebase = db.initialize_app(config)
+
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        
+        
+
     return render_template('login.html')
 
 @app.route('/translate')
 def translate():
     return render_template('translate.html')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('uname')
+        password = request.form.get('psw')
+        c_password = request.form.get('cpsw')
+        enc_pass = fernet.encrypt(password.encode(), c_password.encode())
+
+        if not username or not email or not password or not c_password:
+            error = "please fill in all the required fields."
+            return render_template('register.html')
+
     return render_template('register.html')
 
 
