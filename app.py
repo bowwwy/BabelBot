e28b864bb3aa6450aaf774e87374680bf32f0615
Bear does coding <105804085+Akari-ii@users.noreply.github.com>
2023-07-04 10:35:05 +0800

    minor updates

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from __future__ import unicode_literals, print_function, division
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3 as db
 import FrenchToEngModel as En
 import EngToFrenchModel as Fr
 
 app = Flask(__name__)
+
+
+#print("Opened database successfully")
+#conn = db.connect('test.db')
+#conn.execute('''CREATE TABLE ACC
+#         (USER TEXT PRIMARY KEY     NOT NULL,
+#        PASSWORD           TEXT    NOT NULL,
+#        HISTORY        CHAR(50));''')
+#print("Table created successfully")
+
+#conn.close()
+
 
 @app.route('/')
 def home():
@@ -25,16 +38,21 @@ def register():
 
 @app.route('/translator', methods=['GET', 'POST'])
 def translator():
-    sentence = request.form['text']
-    opt = request.form['options']
-    if opt == 'French':
-        sentence = Fr.normalizeStringFra(sentence)
-        output_words, _ = Fr.evaluateToFra(Fr.encoderEng, Fr.decoderFra, sentence, Fr.input_lang_eng, Fr.output_lang_fra)
-    elif opt == 'English':
-        sentence = En.normalizeString(sentence)
-        output_words, _ = En.evaluateEng(En.encoder, En.decoder, sentence, En.input_lang, En.output_lang)
-    output_sentence = ' '.join(output_words)
-    return render_template('translate.html', in_sentence = sentence, translated_text = output_sentence)
+    try:
+        sentence = request.form['text']
+        opt = request.form['options']
+        if opt == 'French':
+            sentence = Fr.normalizeStringFra(sentence)
+            output_words, _ = Fr.evaluateToFra(Fr.encoderEng, Fr.decoderFra, sentence, Fr.input_lang_eng, Fr.output_lang_fra)
+        elif opt == 'English':
+            sentence = En.normalizeString(sentence)
+            output_words, _ = En.evaluateEng(En.encoder, En.decoder, sentence, En.input_lang, En.output_lang)
+        output_sentence = ' '.join(output_words)
+        return render_template('translate.html', in_sentence = sentence, translated_text = output_sentence)
+    except KeyError:
+       # Edit this and make it as a pop up form 
+       return render_template('translate.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
