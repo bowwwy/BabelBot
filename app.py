a869fc5bec75f1e5da5dd95314561c0b275e3df8
Bear does coding <105804085+Akari-ii@users.noreply.github.com>
2023-07-04 13:37:31 +0800

    initial db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals, print_function, division
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 from cryptography.fernet import Fernet
+from requests.exceptions import HTTPError
+import re
 import pyrebase as db
 import FrenchToEngModel as En
 import EngToFrenchModel as Fr
@@ -18,6 +20,14 @@ config = {"apiKey": "AIzaSyDkiF2t9twh5qfFMziliS7b9nI6pX2c_ag",
 }
 
 firebase = db.initialize_app(config)
+auth = firebase.auth()
+
+def check_user_logged_in():
+    if 'user_id' in session:
+        return True
+    else:
+        return False
+
 
 key = Fernet.generate_key()
 fernet = Fernet(key)
@@ -50,10 +60,35 @@ def register():
         password = request.form.get('psw')
         c_password = request.form.get('cpsw')
         enc_pass = fernet.encrypt(password.encode(), c_password.encode())
-
+        
         if not username or not email or not password or not c_password:
             error = "please fill in all the required fields."
+            #return render_template('register.html', error = error) implement this when modal is added in html
             return render_template('register.html')
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        if not re.match(email_pattern, email):
+            error = 'Invalid email format.'
+            #
+            #return render_template('register_screen.html', error=error) implement in front-end
+            return render_template('register_screen.html')
+
+        password_pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$"
+
+        if not re.match(password_pattern, password):
+            error = 'Password should contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number.'
+            #return render_template('register_screen.html', error=error) implement front end
+            return render_template('register_screen.html')
+        try:
+            user = auth.create_user_with_email_and_password(username, password)
+            return render_template('login')
+        except HTTPError as e:
+            if e.response is not None and e.response.content:
+                error = e.response.json()['error']['message']
+            else:
+                error = 'An error occurred during registration.'
+            #return render_template('register.html', error=error) implement modal
+            return render_template('register.html', error=error)
 
     return render_template('register.html')
 
